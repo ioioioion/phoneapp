93918cd6c133385a17bc7aa62a772d38a9ff46fb
shenwu <wangxulian@gmail.com>
2015-07-17 21:24:12 +0800

    uiui

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@
 	tiles:{ left:10, right:10 }
 	});
 	var sliderThumbSkin = new Skin({ texture: marksTexture, x:70, y:0, width:20, height:30, states:30 });
-    var graySkin = new Skin( { fill:"#555555" } );    
+    var graySkin = new Skin( { fill:"#999999" } );    
     var blueSkin = new Skin( { fill:"#4169E1" } );
     var humiditySkin = new Skin( { fill:"#6A5ACD" } );
     var luminanceSkin = new Skin( { fill:"#FF4040" } );
@@ -24,8 +24,10 @@
     var setHSkin = new Skin( { fill:"#00CD00" } );
     var luminancelabelStyle = new Style( { font: "bold 30px", color:"black" } );
     var labelStyle = new Style( { font: "bold 30px", color:"white" } );
-    var templabelStyle = new Style( { font: "bold 30px", color:"white" } );
-    var bluelabelStyle = new Style( { font: "bold 30px", color:"#4169E1" } );
+    var templabelStyle = new Style( { font: "30px", color:"white" } );
+    var bluelabelStyle = new Style( { font: "30px", color:"#4169E1" } );
+        var bigbluelabelStyle = new Style( { font: "40px", color:"#4169E1" } );
+    
     var sliderLabelStyle = new Style({ font:"bold", size:14, horizontal:"left", color:["white","white","#acd473"] });
 	var sliderValueStyle = new Style({ font:"bold", size:14, horizontal:"right", color:["white","white","#acd473"] });
     var temp = 25;
@@ -48,8 +50,8 @@
     var humidityLabel = new Label({left:0, right:0,bottom:1, height:40, string:"Humidity: "+humidity+" %", style: templabelStyle,skin:blueSkin});
     var tempLabel = new Label({left:0, right:0,bottom:1, height:40,string:"Temperature: "+temp+" °C", style: templabelStyle,skin:blueSkin});
     
-    var adj_hum_Label = new Label({left:0, right:0, height:60, string:"50 %", style: bluelabelStyle});
-    var adj_lum_Label = new Label({left:0, right:0, height:60, string:"200 lx", style: bluelabelStyle});
+    var adj_hum_Label = new Label({left:0, right:0, height:60, string:"50 %", style: bigbluelabelStyle});
+    var adj_lum_Label = new Label({left:0, right:0, height:60, string:"200 lx", style: bigbluelabelStyle});
         var waterButton = BUTTONS.Button.template(function($){ return{
         left: 10, right: 10, top: 0, bottom: 10,skin: blueSkin,
         contents: [
