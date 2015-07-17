bab1f68a401b9eb05354d1283bca346a2a38c974
Nicky Xi <xi.nicky@gmail.com>
2015-07-17 17:07:52 +0800

    finish ui write

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 //@program
-//@program
     var THEME = require("themes/flat/theme");
  //   var THEME = require("themes/sample/theme");
 	var CONTROL = require("mobile/control");
@@ -19,14 +18,16 @@
 	var sliderThumbSkin = new Skin({ texture: marksTexture, x:70, y:0, width:20, height:30, states:30 });
     
     var whiteSkin = new Skin( { fill:"white" } );
-    var labelStyle = new Style( { font: "bold 40px", color:"black" } );
+    var labelStyle = new Style( { font: "bold 30px", color:"black" } );
     var sliderLabelStyle = new Style({ font:"bold", size:14, horizontal:"left", color:["white","white","#acd473"] });
 	var sliderValueStyle = new Style({ font:"bold", size:14, horizontal:"right", color:["white","white","#acd473"] });
-    
+    var temp = 25;
+    var humidity = 70;
+    var luminance = 30.67;
     Handler.bind("/discover", Behavior({
         onInvoke: function(handler, message){
               trace("Found the device.\n");	
-        
+              temp = 30 ;
     //        deviceURL = JSON.parse(message.requestText).url;
         }
     }));
@@ -36,12 +37,30 @@
             deviceURL = "";
         }
     }));
+    var luminanceLabel = new Label({left:0, right:0, height:40, string:"Luminance: "+luminance+" lx", style: labelStyle});
+    var humidityLabel = new Label({left:0, right:0, height:40, string:"Humidity: "+humidity+" %", style: labelStyle});
+    var tempLabel = new Label({left:0, right:0, height:40, string:"Temperature: "+temp+" °C", style: labelStyle});
     
-    var counterLabel = new Label({left:0, right:0, height:40, string:"0", style: labelStyle});
-    var ResetButton = BUTTONS.Button.template(function($){ return{
-        left: 0, right: 0, height:50,
+    var adj_hum_Label = new Label({left:0, right:0, height:60, string:"50 %", style: labelStyle});
+    var adj_lum_Label = new Label({left:0, right:0, height:60, string:"200 lx", style: labelStyle});
+        var waterButton = BUTTONS.Button.template(function($){ return{
+        left: 0, right: 10, top: 0, bottom: 0,
         contents: [
-            new Label({left:0, right:0, height:40, string:"Reset", style: labelStyle})
+            new Label({left:0, right:0, height:40, string:"water", style: labelStyle})
+        ],
+        behavior: Object.create(BUTTONS.ButtonBehavior.prototype, {
+            onTap: { value: function(content){
+                content.invoke(new Message(deviceURL + "water"), Message.JSON);
+            }},
+            onComplete: { value: function(content, message, json){
+                counterLabel.string = json.count;
+            }}
+        })
+    }});
+        var lightButton = BUTTONS.Button.template(function($){ return{
+        left: 10, right: 0, top: 0, bottom: 0,
+        contents: [
+            new Label({left:0, right:0, height:40, string:"light", style: labelStyle})
         ],
         behavior: Object.create(BUTTONS.ButtonBehavior.prototype, {
             onTap: { value: function(content){
@@ -52,20 +71,97 @@
             }}
         })
     }});
-    
+        var actionLine = new Line({
+        left: 10, right: 10, top: 0, bottom: 10, active: true, skin: whiteSkin,
+        contents: [
+            new waterButton(),
+            new lightButton()  
+        ]
+    });
+    var ahButton = BUTTONS.Button.template(function($){ return{
+        width:60 , height:60,
+        contents: [
+            new Label({left:0, right:0, height:40, string:"+", style: labelStyle})
+        ],
+        behavior: Object.create(BUTTONS.ButtonBehavior.prototype, {
+            onTap: { value: function(content){
+                content.invoke(new Message(deviceURL + "reset"), Message.JSON);
+            }},
+            onComplete: { value: function(content, message, json){
+                counterLabel.string = json.count;
+            }}
+        })
+    }});
+        var mhButton = BUTTONS.Button.template(function($){ return{
+        width:60 , height:60,
+        contents: [
+            new Label({left:0, right:0, height:40, string:"-", style: labelStyle})
+        ],
+        behavior: Object.create(BUTTONS.ButtonBehavior.prototype, {
+            onTap: { value: function(content){
+                content.invoke(new Message(deviceURL + "reset"), Message.JSON);
+            }},
+            onComplete: { value: function(content, message, json){
+                counterLabel.string = json.count;
+            }}
+        })
+    }});
+        var alButton = BUTTONS.Button.template(function($){ return{
+        width:60 , height:60,
+        contents: [
+            new Label({left:0, right:0, height:40, string:"+", style: labelStyle})
+        ],
+        behavior: Object.create(BUTTONS.ButtonBehavior.prototype, {
+            onTap: { value: function(content){
+                content.invoke(new Message(deviceURL + "reset"), Message.JSON);
+            }},
+            onComplete: { value: function(content, message, json){
+                counterLabel.string = json.count;
+            }}
+        })
+    }});
+        var mlButton = BUTTONS.Button.template(function($){ return{
+        width:60 , height:60,
+        contents: [
+            new Label({left:0, right:0, height:40, string:"-", style: labelStyle})
+        ],
+        behavior: Object.create(BUTTONS.ButtonBehavior.prototype, {
+            onTap: { value: function(content){
+                content.invoke(new Message(deviceURL + "reset"), Message.JSON);
+            }},
+            onComplete: { value: function(content, message, json){
+                counterLabel.string = json.count;
+            }}
+        })
+    }});
+    var adj_humidiLine = new Line({
+        left: 10, right: 10, height:70, active: true, skin: whiteSkin,
+        contents: [
+            new mhButton(),
+            adj_hum_Label,
+            new ahButton()  
+        ]
+    });
+    var adj_lumLine = new Line({
+        left: 10, right: 10, height:70, active: true, skin: whiteSkin,
+        contents: [
+            new mlButton(),
+            adj_lum_Label,
+            new alButton()  
+        ]
+    });
     var mainColumn = new Column({
         left: 0, right: 0, top: 0, bottom: 0, active: true, skin: whiteSkin,
         contents: [
-            new Label({left:0, right:0, height:40, string:"Counter:", style: labelStyle}),
-            counterLabel,
-            MenuSlider({ log:false, min:0, max:80, step:1, title:"Indentation",
-			getter: function() { 
-				return   0; 
-			},
-			setter: function(value) { 
-			},
-			}),
-            new ResetButton()
+            tempLabel,
+            humidityLabel,
+            luminanceLabel,
+            new Label({left:0, right:0, height:40, string:"Set Humidity", style: labelStyle}),
+            adj_humidiLine,
+            new Label({left:0, right:0, height:40, string:"Set Luminance", style: labelStyle}),
+            adj_lumLine,
+            new Label({left:0, right:0, height:40, string:"Actions", style: labelStyle}),
+            actionLine
         ],
         behavior: Behavior({
             onTouchEnded: function(content){
@@ -86,5 +182,5 @@
         },
     })
     
-    model = application.behavior = new ApplicationBehavior();
+    application.behavior = new ApplicationBehavior();
     application.add(mainColumn);
